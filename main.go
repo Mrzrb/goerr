7fe97c0debf71f5db63e8bcb1a945f6c6623edd4
zhangruobin <zhangruobin@zuoyebang.com>
2024-05-22 11:50:56 +0800

    main

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
-func main() {
-}
+import (
+	_ "github.com/Mrzrb/goerr/annotations/getset"
+	_ "github.com/YReshetko/go-annotation/annotations/constructor"
+	_ "github.com/YReshetko/go-annotation/annotations/mapper"
+	_ "github.com/YReshetko/go-annotation/annotations/validator"
+	annotation "github.com/YReshetko/go-annotation/pkg"
+)
 
-// @Gettter
-type Te struct {
-	This int
-	THat string
+func main() {
+	annotation.Process()
 }
