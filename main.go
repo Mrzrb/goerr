90443b40d31adb0ef0570b6235c2fd8f67d59818
zhangruobin <zhangruobin@zuoyebang.com>
2024-06-03 19:01:08 +0800

    bi

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/Mrzrb/goerr/annotations/aop"
 	_ "github.com/Mrzrb/goerr/annotations/getset"
 	_ "github.com/Mrzrb/goerr/annotations/logger"
+	_ "github.com/Mrzrb/goerr/internal/processors/autowire"
 	_ "github.com/YReshetko/go-annotation/annotations/constructor"
 	_ "github.com/YReshetko/go-annotation/annotations/mapper"
 	_ "github.com/YReshetko/go-annotation/annotations/validator"
